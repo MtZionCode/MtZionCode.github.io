cd7c9d6672101851b6cf51db49a7bc0f4672e8a8
Gebre Mesquitta <23564291+MtZionCode@users.noreply.github.com>
2018-06-13 15:33:43 -0500

    Create profile.js
    Add lights to "Portfolio" navigation button that's based on state of portfolio slideshow animation

---
+++ profile.js
@@ -0,0 +1,12 @@
+//add two lights to "portfolio" button that operates based on status of animation
+
+//showcase is the div you click and figure is the element whose webkit-animation-play-state is getting changed:
+
+  var showcase = document.querySelector("figure"); 
+  showcase.addEventListener("mouseover", function(){ 
+  $(".red").addClass("glow-red"); 
+  $(".green").removeClass("glow-green"); }); 
+  showcase.addEventListener("mouseout", function(){ 
+  $(".red").removeClass("glow-red");
+  $(".green").addClass("glow-green");  
+  });
