e421d0f92d605d2d0eb6f975aa1c806b22e19540
Gebre Mesquitta <23564291+MtZionCode@users.noreply.github.com>
2018-06-13 17:20:41 -0500

    Update profile.js

--- profile.js
+++ profile.js
@@ -3,9 +3,12 @@
 //showcase is the div you click and figure is the element whose webkit-animation-play-state is getting changed:
 
   var showcase = document.querySelector("figure"); 
+
   $(".showcase").mouseover(function(){ 
   $(".red").addClass("glow-red"); 
-  $(".green").removeClass("glow-green"); }); 
+  $(".green").removeClass("glow-green"); 
+  }); 
+
   $(".showcase").mouseout(function(){ 
   $(".red").removeClass("glow-red");
   $(".green").addClass("glow-green");  
