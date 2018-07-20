0815c854a3cc8ce9b26219154c352e76bbd0c4b4
Gebre Mesquitta <23564291+MtZionCode@users.noreply.github.com>
2018-07-20 09:20:43 -0500

    Update profile.js
    Added Typewriter function to a portion of into paragraph

--- profile.js
+++ profile.js
@@ -1,3 +1,49 @@
+
+$(document).ready(function () {
+
+document.addEventListener('DOMContentLoaded',function(event){
+  // array with texts to type in typewriter
+  var dataText = [ "Devoted to the design of experiences with code."];
+  
+  // type one text in the typwriter
+  // keeps calling itself until the text is finished
+  function typeWriter(text, i, fnCallback) {
+    // chekc if text isn't finished yet
+    if (i < (text.length)) {
+      // add next character to h1
+    document.getElementById("one").innerHTML = text.substring(0, i+1) +'<span aria-hidden="true"></span>';
+
+      // wait for a while and call this function again for next character
+      setTimeout(function() {
+        typeWriter(text, i + 1, fnCallback)
+      }, 100);
+    }
+    // text finished, call callback if there is a callback function
+    else if (typeof fnCallback == 'function') {
+      // call callback after timeout
+      setTimeout(fnCallback, 700);
+    }
+  }
+  // start a typewriter animation for a text in the dataText array
+   function StartTextAnimation(i) {
+     if (typeof dataText[i] == 'undefined'){
+        setTimeout(function() {
+          StartTextAnimation(0);
+        }, 20000);
+     }
+     // check if dataText[i] exists
+    if (i < dataText[i].length) {
+      // text exists! start typewriter animation
+     typeWriter(dataText[i], 0, function(){
+       // after callback (and whole text has been animated), start next text
+       StartTextAnimation(i + 1);
+     });
+    }
+  }
+  // start the text animation
+  StartTextAnimation(0);
+});
+
 //lOGIC:
 
   //Part1: add two lights to "portfolio" button that operates based on status of animation - running or paused states
@@ -6,10 +52,7 @@
   //Part 2: make glowing red light flash rapidly when user mouseovers content of "Portfolio" button drop-down menu
 
 //showcase is the div you click and figure is the element whose webkit-animation-play-state is getting changed:
-$(document).ready(function () {
-
-
-
+	
 //part A (of Part 1)
   
  var showcase = document.querySelector("figure"); 
