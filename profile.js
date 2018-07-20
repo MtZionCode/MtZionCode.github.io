1383676c348ab5a6eb66ee2d6fb719af5aa2c381
Gebre Mesquitta <23564291+MtZionCode@users.noreply.github.com>
2018-07-20 10:54:10 -0500

    Update profile.js
    Trying to fix bug causing javascript code to not run: re-added "document.addEventListener('DOMContentLoaded',function(event){});"

--- profile.js
+++ profile.js
@@ -1,6 +1,7 @@
 
 $(document).ready(function () {
-
+	
+document.addEventListener('DOMContentLoaded',function(event){
 
   // array with texts to type in typewriter
   var dataText = [ "Devoted to the design of experiences with code."];
@@ -42,7 +43,8 @@ $(document).ready(function () {
   
   // start the text animation
   StartTextAnimation(0);
-
+  });
+	
 //lOGIC:
 
   //Part1: add two lights to "portfolio" button that operates based on status of animation - running or paused states
